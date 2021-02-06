e0bad6a76548da864394c4285edbee7e24dc1276
Cameron Baughn <cam.baughn@gmail.com>
2021-02-06 05:40:59 -0800

    Remove react router

--- components/App/App.js
+++ components/App/App.js
@@ -1,10 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './App.module.scss';
-import {
-  Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { connect } from 'react-redux';
 
 // Components
 import MainContainer from '../Main/MainContainer';
@@ -13,22 +9,57 @@ import Login from '../Login/Login';
 import SearchContainer from '../Search/SearchContainer';
 
 // Utility Functions
+import { setCard, setSales, setUser } from '../../redux/actionCreators';
+import { searchCard, configureSearchTerm } from '../../util/algolia/algoliaHelpers';
+import pokeCard from '../../util/api/card';
+import { getSalesForCard } from '../../util/api/sales';
+import { getCardInfo } from '../../util/pokemonAPI/pokemonAPI';
 
 
-export default function App({}) {
-  return (
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/search">
-            <SearchContainer />
-          </Route>
-          <Route path="/">
-            <SearchContainer />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        {/* <h2 className={styles.brand}>X-ray</h2> */}
+        <div className={styles.topBanner}>
+          {/* <img src={require('../../assets/images/wordmark.png')} alt={'wordmark'} className={styles.brand} /> */}
+        </div>
+        {/* { !this.props.user.id &&
+          <Login setUser={this.props.setUser} />
+        } */}
 
-  )
+        <SearchContainer />
+        {/* <MainContainer /> */}
+
+        {/* { this.props.user.id && this.props.user.status !== 'subscribed' &&
+          <SubscribeContainer />
+        } */}
+
+      </div>
+    );
+  }
 }
+
+
+
+
+const mapStateToProps = state => {
+  return {
+    user: state.user,
+    card: state.card,
+    sales: state.sales,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setUser: user => dispatch(setUser(user)),
+    setCard: details => dispatch(setCard(details)),
+    setSales: sales => dispatch(setSales(sales))
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);

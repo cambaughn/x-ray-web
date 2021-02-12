ab614ebadef2d50f11787db735d1c2382f94a01a
Cameron Baughn <cam.baughn@gmail.com>
2021-02-11 19:43:37 -0800

    Revert "Attempt vx"
    This reverts commit e00f98ed229ef23eda62ada6c1af331c6bf75f8b.

--- components/PriceChart/PriceChart.js
+++ components/PriceChart/PriceChart.js
@@ -1,49 +1,14 @@
 import React from 'react';
 import styles from './PriceChart.module.scss';
 
-import { LinePath, Line, Bar } from "@vx/shape";
-import { scaleTime, scaleLinear } from "@vx/scale";
-import { localPoint } from "@vx/event";
-import { extent, max, bisector } from "d3-array";
-import { appleStock } from "@vx/mock-data";
-
 // Components
 
 // Utility functions
 
 export default function PriceChart({ sales }) {
-
-  const stock = appleStock.slice(800);
-  const xSelector = d => new Date(d.date);
-  const ySelector = d => d.close;
-  const bisectDate = bisector(xSelector).left;
-
-  const width = 900;
-  const height = 400;
-
-  const xScale = scaleTime({
-    range: [0, width],
-    domain: extent(stock, xSelector),
-  });
-
-  const yMax = max(stock, ySelector);
-
-  const yScale = scaleLinear({
-    range: [height, 0],
-    domain: [0, yMax + (yMax / 4)],
-  });
-
   return (
-    <svg width={width} height={height}>
-      <rect x={0} y={0} width={width} height={height} fill="#32deaa" rx={14} />
-      <LinePath
-        data={stock}
-        xScale={xScale}
-        yScale={yScale}
-        x={xSelector}
-        y={ySelector}
-        strokeWidth={5}
-      />
-    </svg>
+    <div className={styles.container}>
+
+    </div>
   )
 }

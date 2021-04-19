95a85d65c8ef7f0a7e0df23a9ffc15ba8bdd9da9
Cameron Baughn <cam.baughn@gmail.com>
2021-04-18 18:50:33 -0700

    Test dates

--- util/date.test.js
+++ util/date.test.js
@@ -1,9 +1,15 @@
 import { formatDateAsString, formatDateAsStringWithTime } from './date';
 
-describe.only('Verify date helpers', () => {
+describe('Verify date helpers', () => {
   test('Can format a date object as a string', () => {
     let date = new Date(2021, 0, 15);
     expect(formatDateAsString(date)).toBe('2021-0-15');
   });
 
+  test('Can format a date object as a string with time', () => {
+    let date = new Date(2021, 0, 15);
+    date.setHours(4);
+    date.setMinutes(15);
+    expect(formatDateAsStringWithTime(date)).toBe('2021-0-15-4-15');
+  });
 })

473ce32b15a8cd81bb60fa3fe44af138d56375a1
Cameron Baughn <cam.baughn@gmail.com>
2021-04-18 18:47:29 -0700

    Add jest

--- util/date.test.js
+++ util/date.test.js
@@ -1,9 +1,9 @@
 import { formatDateAsString, formatDateAsStringWithTime } from './date';
 
-describe('Verify date helpers', () => {
+describe.only('Verify date helpers', () => {
   test('Can format a date object as a string', () => {
     let date = new Date(2021, 0, 15);
-    expect(formatDateAsString(date)).toBe('2020-01-15');
+    expect(formatDateAsString(date)).toBe('2021-0-15');
   });
 
 })

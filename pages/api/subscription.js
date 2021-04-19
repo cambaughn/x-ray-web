5a5fad261eae63ef98e822b6d3dd730d79beb543
Cameron Baughn <cam.baughn@gmail.com>
2021-04-19 14:17:03 -0700

    Utilize test_stripe_customer_id

--- pages/api/subscription.js
+++ pages/api/subscription.js
@@ -6,11 +6,13 @@ export default async (request, response) => {
 
   try {
     const customer = await stripe.customers.retrieve(customer_id);
+
+    console.log('customer ', customer);
     let subscriptionData = customer.subscriptions.data[0];
     let subscribed = subscriptionData && subscriptionData.status === 'active';
 
     return response.status(200).json({ subscriptionStatus: 'active' })
-  } catch(error) { 
+  } catch(error) {
     console.log(' error    ', error)
     return response.status(400).json({ subscriptionStatus: 'not_subscribed' });
   }

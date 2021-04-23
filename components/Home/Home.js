665c07901b37d76a9fc5dc642cc11a31870f6d71
Cameron Baughn <cam.baughn@gmail.com>
2021-04-22 20:51:28 -0700

    Update messaging on home screen

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Home.module.scss';
+import classNames from 'classnames';
 
 // Components
 import GetStartedButton from '../Buttons/GetStartedButton';
@@ -11,9 +12,8 @@ export default function Home({}) {
     <div className={styles.container}>
       {/* Product Overview */}
       <div className={styles.textBlock}>
-        <h1 className={styles.header}>X-ray vision</h1>
-        <p className={styles.bodyText}>Calling all Pokémon collectors!</p>
-        <p className={styles.bodyText}>X-ray gives you deep insights into current trends and sales history across platforms, enabling you to make smart decisions when selling or buying new cards.</p>
+        <h1 className={styles.header}>Power up your collection</h1>
+        <p className={styles.bodyText}>Pokémon collectors: <span className={styles.callout}>Track the value of your cards with X-ray.</span></p>
       </div>
 
       <div className={styles.getStartedWrapper}>
@@ -26,16 +26,16 @@ export default function Home({}) {
       <div className={styles.textBlock}>
         <div className={styles.featureSection}>
           <img src={'/images/stocks.png'} alt={'stocks icon'} className={styles.featureIcon} />
-          <p className={`${styles.bodyText} ${styles.featuresText}`}>See specifics on individual sales across time and track overall trends on the items you care about.</p>
+          <p className={`${styles.bodyText} ${styles.featuresText}`}>Track sales trends on the cards you care about.</p>
         </div>
 
         <div className={styles.featureSection}>
           <img src={'/images/smart.png'} alt={'brain icon'} className={styles.featureIcon} />
-          <p className={`${styles.bodyText} ${styles.featuresText}`}>Get the info you need to make smart decisions with constantly-updated data from major e-commerce platforms.</p>
+          <p className={`${styles.bodyText} ${styles.featuresText}`}>Make smart buying and selling decisions with constantly-updated data from major e-commerce platforms.</p>
         </div>
         <div className={styles.featureSection}>
           <img src={'/images/details.png'} alt={'shovel icon'} className={styles.featureIcon} />
-          <p className={`${styles.bodyText} ${styles.featuresText}`}>Dig into the specifics with detailed breakdowns by platform, item quality and condition, and grading status.</p>
+          <p className={`${styles.bodyText} ${styles.featuresText}`}>Dig into the specifics with details on grading, volume, and changes over time.</p>
         </div>
       </div>
     </div>

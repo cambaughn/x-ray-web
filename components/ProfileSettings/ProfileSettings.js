5be028a44ad92c1fcc157b251a4982f46d21af37
Cameron Baughn <cam.baughn@gmail.com>
2021-04-24 20:01:56 -0700

    Create profile settings page

---
+++ components/ProfileSettings/ProfileSettings.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './ProfileSettings.module.scss';
+
+// Components
+
+// Utility functions
+
+export default function ProfileSettings({}) {
+  return (
+    <div className={styles.container}>
+      sldfkjdlsf
+    </div>
+  )
+}
---
+++ pages/profile/settings.js
@@ -0,0 +1,8 @@
+import Link from 'next/link';
+import ProfileSettings from '../../components/ProfileSettings/ProfileSettings';
+
+export default function ProfileSettingsPage() {
+  return (
+    <ProfileSettings />
+  )
+}

ab9d5b4c175461b4bf56b74a957e81ed6f65c25e
Cameron Baughn <cam.baughn@gmail.com>
2021-04-30 16:52:02 -0700

    Update image for share

--- components/Head/Head.js
+++ components/Head/Head.js
@@ -24,8 +24,8 @@ const Head = (props) => (
     <meta property="og:description" content={props.description || defaultDescription} />
     <meta name="twitter:site" content={props.url || defaultOGURL} />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={'/static/wordmark.png' || props.ogImage || defaultOGImage} />
-    <meta property="og:image" content={'/static/wordmark.png' || props.ogImage || defaultOGImage} />
+    <meta name="twitter:image" content={'/images/wordmark.png' || props.ogImage || defaultOGImage} />
+    <meta property="og:image" content={'/images/wordmark.png' || props.ogImage || defaultOGImage} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
   </NextHead>

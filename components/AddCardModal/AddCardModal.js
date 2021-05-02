d4e39d0e95934056f12cb0520e087faea1f41402
Cameron Baughn <cam.baughn@gmail.com>
2021-05-02 13:24:52 -0700

    Handle half grades

--- components/AddCardModal/AddCardModal.js
+++ components/AddCardModal/AddCardModal.js
@@ -11,8 +11,30 @@ import classNames from 'classnames';
 export default function AddCardModal({ toggleModal, card }) {
   const [graded, setGraded] = useState(false);
   const [gradingAuthority, setGradingAuthority] = useState('');
+  const [grade, setGrade] = useState(10);
+  const [showHalfGrades, setShowHalfGrades] = useState(false);
 
   const graders = ['PSA', 'BGS', 'CGC', 'Other'];
+
+  const getGrades = () => {
+    let possibleGrades = [];
+
+    // if (gradingAuthority === 'CGC' || gradingAuthority === 'BGS') {
+    //   for (let i = 1; i <= 10; i += 0.5) {
+    //     possibleGrades.push(i);
+    //   }
+    // } else if (gradingAuthority === 'PSA' || gradingAuthority === 'Other') {
+    //   for (let i = 1; i <= 10; i++) {
+    //     possibleGrades.push(i);
+    //   }
+    // }
+
+    for (let i = showHalfGrades ? 9.5 : 10; i >= 1; i--) {
+      possibleGrades.push(i);
+    }
+
+    return possibleGrades;
+  }
 
   const stopClick = (event) => {
     event.stopPropagation();
@@ -36,19 +58,37 @@ export default function AddCardModal({ toggleModal, card }) {
 
         <div className={classNames({ [styles.gradingAuthority]: true, [styles.gradingAuthorityVisible]: graded })}>
 
-          <h4>Grading</h4>
+          <h4 className={styles.gradingTitle}>Grading</h4>
           <div className={styles.gradingAuthorityButtons}>
             { graders.map((grader) => {
               return (
-                <div className={classNames({ [styles.button]: true, [styles.gradingAuthorityButton]: true, [styles.selectedButton]: gradingAuthority === grader })} onClick={() => setGradingAuthority(grader)}>
+                <div className={classNames({ [styles.button]: true, [styles.gradingAuthorityButton]: true, [styles.selectedButton]: gradingAuthority === grader })} onClick={() => setGradingAuthority(grader)} key={grader}>
                   <span>{grader}</span>
                 </div>
               )
             })}
-
           </div>
-
         </div>
+
+        { gradingAuthority.length > 0 && graded &&
+          <div className={classNames({ [styles.gradeSelection]: true, [styles.gradeSelectionVisible]: gradingAuthority.length > 0 })}>
+            <div className={styles.grades}>
+              { getGrades().map(possibleGrade => {
+                return (
+                  <div className={classNames({ [styles.button]: true, [styles.gradeButton]: true, [styles.selectedButton]: possibleGrade === grade })} onClick={() => setGrade(possibleGrade)} key={possibleGrade}>
+                    <span>{possibleGrade}</span>
+                  </div>
+                )
+              })}
+            </div>
+            { (gradingAuthority === 'CGC' || gradingAuthority === 'BGS') &&
+              <div className={classNames({ [styles.button]: true, [styles.halfGradeButton]: true, [styles.selectedButton]: showHalfGrades })} onClick={() => setShowHalfGrades(!showHalfGrades)}>
+                <span>Show .5 grades</span>
+              </div>
+            }
+          </div>
+        }
+
       </div>
     </div>
   );

24a2fb2b98be09685371679573496f9995d11fd8
Cameron Baughn <cam.baughn@gmail.com>
2021-05-09 20:16:57 -0700

    Add chart

--- components/CollectionChart/CollectionChart.js
+++ components/CollectionChart/CollectionChart.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from './CollectionChart.module.scss';
 import { useSelector, useDispatch } from 'react-redux';
+import { Line, defaults } from 'react-chartjs-2';
 
 // Components
 
@@ -126,7 +127,65 @@ export default function CollectionChart({}) {
 
   return (
     <div className={styles.container}>
-      <span>{averagePrice}</span>
+      <Line
+        data={{
+          labels: formattedSales.map(week => week.label),
+          datasets: [
+            {
+              label: 'Price (USD)',
+              data: formattedSales.map(week => week.total),
+              backgroundColor: ['rgba(70, 130, 180, 0.3)', 'rgba(70, 130, 180, 0)'],
+              borderColor: '#4682b4',
+              borderWidth: 2,
+              pointRadius: 0,
+              pointHitRadius: 10,
+            }
+          ]
+        }}
+        // height={isMobile() ? 200 : 300}
+        // width={isMobile() ? 100 : 700}
+        options={{
+          maintainAspectRatio: true,
+          responsive: true,
+          legend: {
+            display: false,
+            labels: {
+              fontColor: 'white',
+              fontSize: 14,
+            }
+          },
+          tooltips: {
+            mode: 'x-axis',
+            callbacks: {
+              label: tooltipItem => `${tooltipItem.xLabel}: $${tooltipItem.yLabel}`,
+              title: () => null,
+            }
+          },
+          scales: {
+            yAxes: [
+              {
+                gridLines: {
+                  color: 'rgba(0, 0, 0, 0)',
+                },
+                ticks: {
+                  beginAtZero: true,
+                  // display: !isMobile() ? true : false
+                }
+              }
+            ],
+            xAxes: [
+              {
+                gridLines: {
+                  color: 'rgba(0, 0, 0, 0)',
+                },
+                ticks: {
+                  display: false //this will remove only the label
+                }
+              }
+            ]
+          }
+        }}
+      />
     </div>
   )
 }

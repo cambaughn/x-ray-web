d4309e9d0aa31381f24c37254baa1c3620c0af55
Cameron Baughn <cam.baughn@gmail.com>
2021-05-16 00:50:17 -0700

    Add and style links

--- components/HamburgerMenu/HamburgerMenu.js
+++ components/HamburgerMenu/HamburgerMenu.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from './HamburgerMenu.module.scss';
-import { Menu } from 'react-feather';
+import { Menu, X } from 'react-feather';
+import Link from 'next/link';
 import classNames from 'classnames';
 
 // Components
@@ -19,10 +20,28 @@ export default function HamburgerMenu({}) {
       <div className={styles.menuButton} onClick={toggleMenu}>
         <Menu className={styles.menuIcon} size={30} />
       </div>
-      
+
       <div className={classNames({ [styles.backdrop]: true, [styles.backdropVisible]: showMenu })} onClick={toggleMenu}></div>
 
       <div className={classNames({ [styles.drawer]: true, [styles.drawerOpen]: showMenu })}>
+        <div className={styles.closeButtonWrapper}>
+          <div className={styles.closeButton} onClick={toggleMenu}>
+            <X size={30} />
+          </div>
+        </div>
+
+        <div className={styles.links}>
+          <Link href={'/'}>
+            <div className={styles.linkWrapper} onClick={toggleMenu}>
+              <span className={styles.link}>Home</span>
+            </div>
+          </Link>
+          <Link href={'/profile/settings'}>
+            <div className={styles.linkWrapper} onClick={toggleMenu}>
+              <span className={styles.link}>Settings</span>
+            </div>
+          </Link>
+        </div>
 
       </div>
 

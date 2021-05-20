363aeb5f4a5012a4df02a3d95e5b9275769130ec
Cameron Baughn <cam.baughn@gmail.com>
2021-05-20 10:24:35 -0700

    Set up browse page

---
+++ components/Browse/Browse.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import styles from './Browse.module.scss';
+
+// Components
+
+// Utility functions
+
+export default function Browse({}) {
+  return (
+    <div className={styles.container}>
+asdf
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import Search from '../components/Search/Search';
 import UserCollection from '../components/UserCollection/UserCollection';
 import Home from '../components/Home/Home';
+import Browse from '../components/Browse/Browse';
 
 export default function Index({}) {
   const user = useSelector(state => state.user);
@@ -11,7 +12,7 @@ export default function Index({}) {
   return (
     <>
       { !!user.email && !!user.username
-        ? (isBetaUser ? <UserCollection /> : <Search />)
+        ? <Browse />
         : <Home />
       }
     </>

389d9da25833f59e0c3eb9fd039f2044ad269347
Cameron Baughn <cam.baughn@gmail.com>
2021-05-20 11:29:23 -0700

    Update series

--- components/Browse/Browse.js
+++ components/Browse/Browse.js
@@ -1,14 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import styles from './Browse.module.scss';
+import { useSelector, useDispatch } from 'react-redux';
 
 // Components
 
 // Utility functions
+import pokeSeries from '../../util/api/series';
+import pokeSet from '../../util/api/set';
+import { setPokemonSet, setPokemonSeries } from '../../redux/actionCreators';
+
 
 export default function Browse({}) {
+  const pokemonSeries = useSelector(state => state.pokemonSeries);
+  const pokemonSet = useSelector(state => state.pokemonSet);
+  const dispatch = useDispatch();
+
+  const getSeriesInfo = async () => {
+    let seriesInfo = await pokeSeries.get();
+    dispatch(setPokemonSeries(seriesInfo));
+  }
+
+  const getSetInfo = async () => {
+    let setInfo = await pokeSet.get();
+    dispatch(setPokemonSet(setInfo));
+  }
+
+  useEffect(getSeriesInfo, []);
+  useEffect(getSetInfo, []);
+
   return (
     <div className={styles.container}>
-asdf
+
     </div>
   )
 }
--- util/api/series.js
+++ util/api/series.js
@@ -1,13 +1,55 @@
 import db from '../firebase/firebaseInit';
+import { convertSnapshot, convertDoc } from './general';
+import pokeSet from './set';
 
 const pokeSeries = {};
 
-pokeSeries.create = async (id, newSeries) => {
+pokeSeries.create = async (newSeries) => {
   try {
-    return db.collection('en_pokemon_series').doc(id).set(newSeries);
+    return db.collection('pokemon_series').doc(newSeries.name).set(newSeries);
   } catch(error) {
     console.error(error);
   }
 }
+
+pokeSeries.get = async (id) =>  {
+  try {
+    if (id) { // get specific set
+      return db.collection('pokemon_series').doc(id).get()
+      .then(function(doc) {
+        return convertDoc(doc);
+      })
+    } else { // get all sets
+      return db.collection('pokemon_series').get()
+      .then(function(snapshot) {
+        let sets = convertSnapshot(snapshot);
+        return sets;
+      })
+    }
+  } catch(error) {
+    console.error(error);
+  }
+}
+
+
+const migrateSeries = async () => {
+  let setInfo = await pokeSet.get();
+  let uniqueSeries = new Set();
+  setInfo.forEach(set => {
+    uniqueSeries.add(set.series);
+    set.series === 'Other' && console.log(set);
+  })
+  let series = Array.from(uniqueSeries);
+  series = series.map(name => {
+    return { name, logo: '' }
+  })
+
+  // series.forEach(item => {
+  //   pokeSeries.create(item);
+  // })
+
+  // console.log('series ', series);
+}
+
 
 export default pokeSeries;

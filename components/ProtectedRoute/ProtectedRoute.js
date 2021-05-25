0376b56f8d59e9a3d9c29c2511f8fbaeacb52f28
Cameron Baughn <cam.baughn@gmail.com>
2021-05-25 09:53:29 -0700

    Start protected route

---
+++ components/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+// Components
+
+// Utility functions
+
+export default function ProtectedRoute({ children }) {
+  return (
+    <>
+      { children }
+    </>
+  )
+}

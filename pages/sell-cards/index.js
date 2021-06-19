286d23ca2509229d8935cbc8ba4a26b19228a9cb
Cameron Baughn <cam.baughn@gmail.com>
2021-06-19 15:40:34 -0700

    Add sell cards button

---
+++ pages/sell-cards/index.js
@@ -0,0 +1,9 @@
+// import Offer from '../../components/Offer/Offer';
+import { useSelector } from 'react-redux';
+import { useRouter } from 'next/router';
+
+
+export default function OfferPage() {
+  // return <Offer />
+  return null
+}

a26bd08b5df4e15108a942ac11bda1633f3043ec
Cameron Baughn <cam.baughn@gmail.com>
2021-06-20 16:45:47 -0700

    Fill in details

--- components/SellCards/SellCards.js
+++ components/SellCards/SellCards.js
@@ -28,15 +28,23 @@ export default function SellCards({}) {
     <div className={styles.container}>
       <h3 className={styles.header}>Sell your cards</h3>
 
+
       { collectionDetails.map((detail, index) => {
-        let item = collectedItems[detail.item_id];
+        let item = collectedItems[detail.item_id] || {};
+        let image_src = null;
+        if (item && item.images) {
+          image_src = item.images.small || item.images.large;
+        }
 
         return (
-          <div className={classNames(styles.itemRow, { [styles.selectedRow]: selected.has(detail.id) })} onClick={() => toggleSelect(detail.id)} key={item.id + index}>
-            {/* TODO: Fix item.images undefined error */}
-            <img src={item && item.images && item.images.small ? item.images.small : item.images.large} className={styles.cardImage} />
-            <span className={styles.itemName}>{item.name}</span>
-            <span className={styles.itemLanguage}>{item.language === 'en' ? 'English' : 'Japanese'}</span>
+          <div className={classNames(styles.itemRow, { [styles.selectedRow]: selected.has(detail.id) })} onClick={() => toggleSelect(detail.id)} key={index}>
+            <img src={image_src} className={styles.cardImage} />
+            <span className={classNames(styles.detailCell, styles.itemName)}>{item.name}</span>
+            <span className={classNames(styles.detailCell, styles.grade)}>{detail.grading_authority && detail.grade ? `${detail.grading_authority} ${detail.grade}` : 'Ungraded'}</span>
+            <span className={classNames(styles.detailCell, styles.setName)}>{item.set_name}</span>
+            <span className={classNames(styles.detailCell, styles.number)}>#{item.number}</span>
+
+            <span className={classNames(styles.detailCell, styles.language)}>{item.language === 'en' ? 'English' : 'Japanese'}</span>
           </div>
         )
       })}

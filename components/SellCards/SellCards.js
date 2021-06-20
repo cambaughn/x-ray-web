1585f5fb9eafd8f1646c01a2b30f45b5c4656a82
Cameron Baughn <cam.baughn@gmail.com>
2021-06-19 18:57:08 -0700

    Toggle selection of items

--- components/SellCards/SellCards.js
+++ components/SellCards/SellCards.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from './SellCards.module.scss';
 import { useSelector, useDispatch } from 'react-redux';
+import classNames from 'classnames';
 
 // Components
 
@@ -10,6 +11,18 @@ export default function SellCards({}) {
   const user = useSelector(state => state.user);
   const collectionDetails = useSelector(state => state.collectionDetails); // array of card ids
   const collectedItems = useSelector(state => state.collectedItems); // object with card objects mapped to ids
+  const [selected, setSelected] = useState(new Set());
+
+  const toggleSelect = (detail_id) => {
+    let newSelected = new Set(selected);
+    if (newSelected.has(detail_id)) {
+      newSelected.delete(detail_id);
+    } else {
+      newSelected.add(detail_id);
+    }
+
+    setSelected(newSelected);
+  }
 
   return (
     <div className={styles.container}>
@@ -19,8 +32,11 @@ export default function SellCards({}) {
         let item = collectedItems[detail.item_id];
 
         return (
-          <div className={styles.itemRow} key={item.id + index}>
+          <div className={classNames(styles.itemRow, { [styles.selectedRow]: selected.has(detail.id) })} onClick={() => toggleSelect(detail.id)} key={item.id + index}>
+            {/* TODO: Fix item.images undefined error */}
+            <img src={item && item.images && item.images.small ? item.images.small : item.images.large} className={styles.cardImage} />
             <span className={styles.itemName}>{item.name}</span>
+            <span className={styles.itemLanguage}>{item.language === 'en' ? 'English' : 'Japanese'}</span>
           </div>
         )
       })}

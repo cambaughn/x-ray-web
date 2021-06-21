56f3773515bfef4ba6427def29597a7e57253b7c
Cameron Baughn <cam.baughn@gmail.com>
2021-06-20 17:35:09 -0700

    Add blank value check page

---
+++ components/ValueCheck/ValueCheck.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import styles from './ValueCheck.module.scss';
+
+// Components
+
+// Utility functions
+
+export default function ValueCheck({}) {
+  return (
+    <div className={styles.container}>
+
+    </div>
+  )
+}

8142ec4b914a5bac8a9ee9e720a5aa20eda3664f
Cameron Baughn <cam.baughn@gmail.com>
2021-06-23 19:50:41 -0700

    Create footer component

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -4,6 +4,7 @@ import classNames from 'classnames';
 
 // Components
 import GetStartedButton from '../Buttons/GetStartedButton';
+import Footer from '../Footer/Footer';
 
 // Utility functions
 
@@ -39,20 +40,7 @@ export default function Home({}) {
         </div>
       </div>
 
-      <div className={styles.socialBlock}>
-        <h3 className={styles.socialText}>Join the conversation</h3>
-
-        <div className={styles.socialLinks}>
-          <a href={'https://discord.gg/Dfqemg8fAV'} target={'_blank'} className={styles.socialLink}>
-            <img src={'/images/discord-logo-black.png'} alt={'discord icon'} className={styles.socialIcon} />
-          </a>
-
-          <a href={'https://twitter.com/xraydotfun'} target={'_blank'} className={styles.socialLink}>
-            <img src={'/images/twitter-logo-black.png'} alt={'twitter icon'} className={classNames(styles.socialIcon, styles.twitterIcon)} />
-          </a>
-        </div>
-
-      </div>
+      <Footer />
     </div>
   )
 }

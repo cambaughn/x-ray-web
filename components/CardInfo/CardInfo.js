3b6aa56918b1930c249b771f37cac408e2f43989
Cameron Baughn <cam.baughn@gmail.com>
2021-06-26 20:13:17 -0700

    Add card detail info to its own component

---
+++ components/CardInfo/CardInfo.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import styles from './CardInfo.module.scss';
+
+// Components
+import AddToCollectionButton from '../Buttons/AddToCollectionButton';
+
+// Utility functions
+
+export default function CardInfo({ card, set, setEditingName, editingName, cardName, setCardName, toggleCardAddition, showHelpText }) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageWrapper}>
+        { card.images && (card.images.small || card.images.large) &&
+          <img src={card.images.small || card.images.large} className={styles.image} />
+        }
+      </div>
+
+      { editingName ? (
+        <input type='text' className={styles.cardName} value={cardName} onChange={(event) => setCardName(event.target.value)} onKeyDown={handleEnterKey} />
+      ): (
+        <h3 className={styles.cardName} onDoubleClick={() => user.role === 'admin' && setEditingName(true)}>{cardName}</h3>
+      )}
+
+      <div className={styles.addButtonWrapper}>
+        <AddToCollectionButton handleClick={toggleCardAddition} showHelpText={showHelpText} />
+      </div>
+
+      { card.name &&
+        <>
+          <div className={styles.cardData}>
+            <span className={styles.label}>Set</span>
+            <span className={styles.detail}>{card.set_name}</span>
+
+            <span className={styles.label}>Number</span>
+            <span className={styles.detail}>{card.number}/{set.printedTotal}</span>
+
+            <span className={styles.label}>Rarity</span>
+            <span className={styles.detail}>{card.rarity}</span>
+          </div>
+
+        </>
+      }
+    </div>
+  )
+}

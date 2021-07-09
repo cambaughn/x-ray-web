ed5ddd574a07bbfc63654e3550bd3a96bccc14f6
Cameron Baughn <cam.baughn@gmail.com>
2021-07-09 00:23:36 -0700

    Render PSA pop

--- components/PSAPopReport/PSAPopReport.js
+++ components/PSAPopReport/PSAPopReport.js
@@ -5,16 +5,25 @@ import styles from './PSAPopReport.module.scss';
 
 // Utility functions
 import psaPopReport from '../../util/api/psaPopReport.js';
+import { capitalize } from '../../util/helpers/string.js';
 
 
 export default function PSAPopReport({ card }) {
   const [reports, setReports] = useState({});
 
+  const grades = ['total', 10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+  const finishes = ['holo', 'reverse_holo', 'non-holo'];
+  const finishMap = { holo: 'Holo', reverse_holo: 'Reverse Holo', 'non-holo': 'Non-Holo' }
+
   const getReports = async () => {
     if (card.id) {
       let reportsForCard = await psaPopReport.search('card_id', card.id);
-      console.log('got reports ', reportsForCard);
-
+      let reportsLookup = {};
+      reportsForCard.forEach(report => {
+        reportsLookup[report.finish] = report;
+      })
+      console.log('got reports ', reportsLookup);
+      setReports(reportsLookup);
     }
   }
 
@@ -22,7 +31,40 @@ export default function PSAPopReport({ card }) {
 
   return (
     <div className={styles.container}>
+      { Object.keys(reports).length > 0 &&
+        <h3 className={styles.title}>PSA Population Report</h3>
+      }
 
+      { finishes.filter(finish => !!reports[finish]).map(finish => {
+        let total = reports[finish].population.total;
+
+        return (
+          <div className={styles.finishWrapper} key={finish}>
+            <span className={styles.finishTitle}>{finishMap[finish] || capitalize(finish)}</span>
+
+            <div className={styles.grades}>
+              {/* Keys */}
+              <div className={styles.gradeBlock}>
+                <span className={styles.gradeLabel}>Grade</span>
+                <span className={styles.number}>#</span>
+                <span className={styles.percentage}>%</span>
+              </div>
+
+              { grades.map((grade, index) => {
+                let numberForGrade = reports[finish].population[grade];
+                let percentage = ((numberForGrade / total) * 100).toFixed(1);
+                return (
+                  <div className={styles.gradeBlock} key={index}>
+                    <span className={styles.gradeLabel}>{grade === 'total' ? 'Total' : grade}</span>
+                    <span className={styles.number}>{numberForGrade}</span>
+                    <span className={styles.percentage}>{percentage}</span>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        )
+      })}
     </div>
   )
 }

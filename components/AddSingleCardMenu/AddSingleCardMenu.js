80f0fb4eb517ce323d537f7531ea89e01165297e
Cameron Baughn <cam.baughn@gmail.com>
2021-07-31 00:49:38 -0700

    Get add card menu to render in action modal

--- components/AddSingleCardMenu/AddSingleCardMenu.js
+++ components/AddSingleCardMenu/AddSingleCardMenu.js
@@ -16,7 +16,7 @@ const finishMap = {
   'holo': 'Holo'
 }
 
-export default function AddSingleCardMenu({ toggleModal, card, finishes }) {
+export default function AddSingleCardMenu({ card = {}, finishes = [] }) {
   const [selectedFinish, setSelectedFinish] = useState(finishes[0] || 'holo'); // holo, non-holo, reverse_holo
   const [graded, setGraded] = useState(false);
   const [gradingAuthority, setGradingAuthority] = useState('PSA');
@@ -110,81 +110,79 @@ export default function AddSingleCardMenu({ toggleModal, card, finishes }) {
   useEffect(getGrades, [gradingAuthority, showHalfGrades]);
 
   return (
-    <div className={styles.container} onClick={toggleModal}>
-      <div className={styles.card} onClick={stopClick}>
-        <h2 className={styles.title}>{card.name}</h2>
+    <div className={styles.container}>
+      <h2 className={styles.title}>{card.name}</h2>
 
-        <h4 className={styles.gradingTitle}>Finish</h4>
-        <div className={styles.finishButtons}>
-          { finishes.map(finish => {
+      <h4 className={styles.gradingTitle}>Finish</h4>
+      <div className={styles.finishButtons}>
+        { finishes.map(finish => {
+          return (
+            <div className={classNames({ [styles.button]: true, [styles.finishButton]: true, [styles.selectedButton]: selectedFinish === finish })} onClick={() => setSelectedFinish(finish)} key={finish}>
+              <span>{finishMap[finish]}</span>
+            </div>
+          )
+        })}
+      </div>
+
+
+      <h4 className={styles.gradingTitle}>Grading</h4>
+
+      <div className={styles.gradedButtons}>
+        <div className={classNames({ [styles.button]: true, [styles.gradedButton]: true, [styles.selectedButton]: graded === false })} onClick={() => setGraded(false)}>
+          <span>Ungraded</span>
+        </div>
+
+        <div className={classNames({ [styles.button]: true, [styles.gradedButton]: true, [styles.selectedButton]: graded === true })} onClick={() => setGraded(true)}>
+          <span>Graded</span>
+        </div>
+      </div>
+
+
+      <div className={classNames({ [styles.gradingAuthority]: true, [styles.gradingAuthorityVisible]: graded })}>
+        <div className={styles.gradingAuthorityButtons}>
+          { graders.map((grader) => {
             return (
-              <div className={classNames({ [styles.button]: true, [styles.finishButton]: true, [styles.selectedButton]: selectedFinish === finish })} onClick={() => setSelectedFinish(finish)} key={finish}>
-                <span>{finishMap[finish]}</span>
+              <div className={classNames({ [styles.button]: true, [styles.gradingAuthorityButton]: true, [styles.selectedButton]: gradingAuthority === grader })} onClick={() => setGradingAuthority(grader)} key={grader}>
+                <span>{grader}</span>
               </div>
             )
           })}
         </div>
 
+        {/* { gradingAuthority === 'Other' &&
+          <input
+            type='text'
+            value={otherGrader}
+            className={styles.otherGraderInput}
+            onChange={event => setOtherGrader(event.target.value)}
+            placeholder='Grader...'
+          />
+        } */}
+      </div>
 
-        <h4 className={styles.gradingTitle}>Grading</h4>
-
-        <div className={styles.gradedButtons}>
-          <div className={classNames({ [styles.button]: true, [styles.gradedButton]: true, [styles.selectedButton]: graded === false })} onClick={() => setGraded(false)}>
-            <span>Ungraded</span>
-          </div>
-
-          <div className={classNames({ [styles.button]: true, [styles.gradedButton]: true, [styles.selectedButton]: graded === true })} onClick={() => setGraded(true)}>
-            <span>Graded</span>
-          </div>
-        </div>
-
-
-        <div className={classNames({ [styles.gradingAuthority]: true, [styles.gradingAuthorityVisible]: graded })}>
-          <div className={styles.gradingAuthorityButtons}>
-            { graders.map((grader) => {
+      { gradingAuthority.length > 0 && graded &&
+        <div className={classNames({ [styles.gradeSelection]: true, [styles.gradeSelectionVisible]: gradingAuthority.length > 0 })}>
+          <div className={styles.grades}>
+            { possibleGrades.map(possibleGrade => {
               return (
-                <div className={classNames({ [styles.button]: true, [styles.gradingAuthorityButton]: true, [styles.selectedButton]: gradingAuthority === grader })} onClick={() => setGradingAuthority(grader)} key={grader}>
-                  <span>{grader}</span>
+                <div className={classNames({ [styles.button]: true, [styles.gradeButton]: true, [styles.selectedButton]: possibleGrade === grade })} onClick={() => setGrade(possibleGrade)} key={possibleGrade}>
+                  <span>{possibleGrade}</span>
                 </div>
               )
             })}
           </div>
-
-          {/* { gradingAuthority === 'Other' &&
-            <input
-              type='text'
-              value={otherGrader}
-              className={styles.otherGraderInput}
-              onChange={event => setOtherGrader(event.target.value)}
-              placeholder='Grader...'
-            />
-          } */}
-        </div>
-
-        { gradingAuthority.length > 0 && graded &&
-          <div className={classNames({ [styles.gradeSelection]: true, [styles.gradeSelectionVisible]: gradingAuthority.length > 0 })}>
-            <div className={styles.grades}>
-              { possibleGrades.map(possibleGrade => {
-                return (
-                  <div className={classNames({ [styles.button]: true, [styles.gradeButton]: true, [styles.selectedButton]: possibleGrade === grade })} onClick={() => setGrade(possibleGrade)} key={possibleGrade}>
-                    <span>{possibleGrade}</span>
-                  </div>
-                )
-              })}
+          { gradingAuthority !== 'PSA' &&
+            <div className={classNames({ [styles.button]: true, [styles.halfGradeButton]: true, [styles.selectedButton]: showHalfGrades })} onClick={() => setShowHalfGrades(!showHalfGrades)}>
+              <span>Show .5 grades</span>
             </div>
-            { gradingAuthority !== 'PSA' &&
-              <div className={classNames({ [styles.button]: true, [styles.halfGradeButton]: true, [styles.selectedButton]: showHalfGrades })} onClick={() => setShowHalfGrades(!showHalfGrades)}>
-                <span>Show .5 grades</span>
-              </div>
-            }
-          </div>
-        }
-
-        <div className={classNames(styles.button, styles.saveButton)} onClick={!submitted ? handleSave : null}>
-          <span>Add to collection</span>
+          }
         </div>
+      }
 
+      <div className={classNames(styles.button, styles.saveButton)} onClick={!submitted ? handleSave : null}>
+        <span>Add to collection</span>
       </div>
+
     </div>
   );
 }

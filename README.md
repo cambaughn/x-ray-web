3c35abba412f32629ffdd090028df6baf2b4ed1b
Cameron Baughn <cam.baughn@gmail.com>
2021-10-27 04:02:06 -0700

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # x-ray-web
+
+- Install packages: `yarn`
+- Start development server: `npm run dev`
+- Run tests: `npm test`
---
+++ util/notion/notionSecrets.js
@@ -0,0 +1,6 @@
+const notion_api_key = "secret_jtAceVT7wHOH2ydQZ5TNIfHHB1EAq5Rpe0jcqGEpMwM";
+const cardsId = "c08aa778968749e686ce2c74c1bb0bfe";
+const setsId = "90881da813b74efb850b68e5bc300ed2";
+const seriesId = "af1837e2fd164f16a27de8ad8695b00a";
+
+export { notion_api_key, cardsId, setsId, seriesId }
